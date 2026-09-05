789ef48c9bb89b875a3506f302ffe86ebc76dc39
drakomanios <drakomanios@gmail.com>
2026-09-05 19:14:35 +0300

    initial _compute_reward function

--- src/go2_stand_up/go2_stand_up.py
+++ src/go2_stand_up/go2_stand_up.py
@@ -22,23 +22,58 @@ class LegAssociation(Enum):
     RL = 2
     RR = 3
 
-
 class NewEnv(QuadrupedEnv):
-    def __init__(self, robot, state_obs_names = ..., scene = 'flat', sim_dt = 0.002, base_vel_command_type = 'forward', ref_base_lin_vel = 0.5, ref_base_ang_vel = 0, ground_friction_coeff = 1, legs_order = ('FL', 'FR', 'RL', 'RR'), sensors = None, sensors_kwargs = None, external_disturbances_kwargs = None,leg_pair = (LegAssociation.FL.value,LegAssociation.RR.value)):
+    def __init__(self, robot, state_obs_names = ..., scene = 'flat', sim_dt = 0.002, base_vel_command_type = 'forward', ref_base_lin_vel = 0.5, ref_base_ang_vel = 0, ground_friction_coeff = 1, legs_order = ('FL', 'FR', 'RL', 'RR'), sensors = None, sensors_kwargs = None, external_disturbances_kwargs = None,
+                 leg_pair = (LegAssociation.FL.value,LegAssociation.RR.value)):
         super().__init__(robot, state_obs_names, scene, sim_dt, base_vel_command_type, ref_base_lin_vel, ref_base_ang_vel, ground_friction_coeff, legs_order, sensors, sensors_kwargs, external_disturbances_kwargs)
-        self.leg_pair = legs_order[leg_pair[0]],legs_order[leg_pair[1]]
+        self.leg_pair = leg_pair
+
+    def distance_from_line_2D(self,p1:NDArray,p2:NDArray,p3:NDArray)->float:
+        """
+        Calculates the distance of the 2D point p3 from the 2D line defined by the points p1, p2
+        """
+        x1 = p1[0]
+        x2 = p2[0]
+        y1 = p1[1]
+        y2 = p2[1]
+
+        numerator = abs((y2-y1)*p3[0] - (x2-x1)*p3[1] + x2*y1 - y2*x1)
+        denumerator = ((y2-y1)**2 + (x2-x1)**2)**0.5
+
+        return numerator/denumerator
 
     def _compute_reward(self):
-        counter = 0
+
         linear_velocity_error = env.base_lin_vel_err(frame="base")
         angular_velocity_error = env.base_ang_vel_err(frame="base")
-        feet_contact_dictionary = env.feet_contact_state(frame="base",ground_reaction_forces=False)[0]
-        for value in feet_contact_dictionary:
-            print(value)
-            if value:
-                counter += 1
-        feet_contact_penalty = 1 if counter != 2 else 0
-        return 1
+        leg_contacts, contact_positions = env.feet_contact_state(frame="base",ground_reaction_forces=False)
+        leg_contacts = leg_contacts.to_list()
+        contact_count = sum(leg_contacts)
+        feet_contact_penalty = 0
+        com_offset = 1
+        # print((contact_count !=2) ,(not leg_contacts[self.leg_pair[0]]),(not leg_contacts[self.leg_pair[1]]))
+        if (contact_count!= 2) or (not leg_contacts[self.leg_pair[0]]) or (not leg_contacts[self.leg_pair[1]]):
+            feet_contact_penalty = 1
+        else:
+            com_xy = env.com[0:2]
+            contact_point2D_1 = contact_positions["FL"] # fix
+            contact_point2D_2 = contact_positions["RR"]
+            # try:
+            contact_point2D_1 = contact_point2D_1[0].pos[0:2]
+            # print(contact_point2D_1)
+            contact_point2D_2 = contact_point2D_2[0].pos[0:2]
+            com_offset = self.distance_from_line_2D(contact_point2D_1,contact_point2D_2,com_xy)
+            # except:
+            # com_offset = 1
+            # print("not in contact")
+
+        return (
+            1 * np.linalg.norm(linear_velocity_error) +
+            1 * np.linalg.norm(angular_velocity_error) +
+            -1 * feet_contact_penalty +
+            -1 * com_offset
+            )
+    
 
 IMU.__init__ = patched_imu_init
 
