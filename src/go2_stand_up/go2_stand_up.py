3b266cbf092d3e747e9091e075af8f79cb589202
FanisSamaras <fanissamaras1@gmail.com>
2026-09-13 19:33:19 +0300

    Parto Parto sou edwsa etoimh thn gh sto piato

--- src/go2_stand_up/go2_stand_up.py
+++ src/go2_stand_up/go2_stand_up.py
@@ -1,5 +1,6 @@
-from envs import create_env
+from envs import create_env,FULL_STATE_OBS
 from internal_control.PID import PIDController
+from policies.PPO import PPO,RolloutBuffer
 import numpy as np
 import torch
 from numpy.typing import NDArray
@@ -7,6 +8,9 @@ from enum import Enum
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def flatten_obs(obs,keys = FULL_STATE_OBS):
+    return np.concatenate([np.atleast_1d(obs[key]) for key in keys])
 
 class LegAssociation(Enum):
     FL = 0
@@ -19,6 +23,49 @@ env = create_env()
 obs = env.reset()
 
 episode_reward = 0.0
+
+OBS_DIM = 37
+ACTION_DIM = 12
+MAX_STEPS = 2_000_000
+EPISODE_LENGTH = 1000
+SAVE_INTERVAL = 50_000
+
+action_scale = 0
+
+agent = PPO(
+    obs_dim=OBS_DIM,
+    action_dim=ACTION_DIM,
+    device=device,
+    lr=1e-4,
+    gamma=0.98,
+    gae_lambda=0.95,
+    clip_eps=0.2,
+    value_coef=0.5,
+    entropy_coef=0.005,
+    max_grad_norm=0.5,
+    ppo_epochs=5,
+    minibatch_size=256)
+
+buffer = RolloutBuffer(
+    obs_dim=OBS_DIM,
+    action_dim=ACTION_DIM,
+    rollout_steps=4096,
+    device=device
+)
+
+obs = flatten_obs(env.reset())
+print(obs.shape)
+
+class Episode:
+    def __init__(self,returns, length_counter, number):
+        self.returns = returns
+        self.length_counter = length_counter
+        self.number = number
+
+episode = Episode(0,0,0)
+
+latest_losses = {}
+
 
 for t in range(1000):
 
